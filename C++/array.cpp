14dcb0ecc174816e8afe701745dfc491a37dc017
“YOUR” <sasanumac@gmail.com>
2023-12-01 00:15:09 -0500

    Added C and C++ practice folder

---
+++ C++/array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int addNum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "Hello world" << endl;
+    
+    int c = 3;
+
+    int someVal = addNum(1, 2);
+
+    cout << someVal << endl;
+
+}
+
